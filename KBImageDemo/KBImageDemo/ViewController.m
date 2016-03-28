809462ffc85c08ceefb0cb4f7b4f0f8b3e8e481e
kangbing <kangbing@kangbingdeMacBook-Air.local>
2016-03-28 16:26:15 +0800

    03-测试

--- KBImageDemo/KBImageDemo/ViewController.m
+++ KBImageDemo/KBImageDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+ImageClip.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 220, 220)];
+    
+    imageView.center = self.view.center;
+    
+    [self.view addSubview:imageView];
+    
+    
+    // 设置图片
+    UIImage *image = [UIImage imageNamed:@"demo.jpg"];
+    
+    [image kb_cornerImageWithCorlor:[UIColor whiteColor] imageSize:imageView.bounds.size completion:^(UIImage *image) {
+        
+        imageView.image = image;
+        
+    }];
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
